8c5f4950ffa7a7f680ff6cda3bd02c17d247940a
Richard Lee <richardlee@Richards-MacBook-Pro.local>
2026-08-19 16:30:18 +0100

    chore: stop tracking Maven target directory

--- target/classes/db/migration/V2__create_transaction_record_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE transaction_record (
-    id BIGSERIAL PRIMARY KEY,
-    type VARCHAR(50) NOT NULL,
-    amount NUMERIC(19, 2) NOT NULL,
-    from_account_id BIGINT,
-    to_account_id BIGINT,
-    created_at TIMESTAMP NOT NULL
-);
